48b0a7dc054347589c9a199ee5b056298f36944c
Aika-Shioiti <aikas0418@gmail.com>
2025-05-07 10:58:30 +0900

    Initial Commit

---
+++ GitHubSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubSample
+//
+//  Created by 塩入愛佳 on 2025/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHubSampleTests/GitHubSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSampleTests.swift
+//  GitHubSampleTests
+//
+//  Created by 塩入愛佳 on 2025/05/07.
+//
+
+import Testing
+@testable import GitHubSample
+
+struct GitHubSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
